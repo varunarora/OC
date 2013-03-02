0292152bac24dacaf8900275f29ab53397219869
Varun Arora <varunarora.mct@gmail.com>
2013-03-02 15:01:41 -0500

    DB settings updated to point to RDB instance

--- oc_platform/settings.py
+++ oc_platform/settings.py
@@ -1,6 +1,4 @@
-# Django settings for oc_platform project.
-
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -12,35 +10,13 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ocrootu',                      # Or path to database file if using sqlite3.
-        'USER': 'ocrootu',                      # Not used with sqlite3.
-        'PASSWORD': 'OCProD7!',                  # Not used with sqlite3.
-        'HOST': 'ocrootu.db.8981201.hostedresource.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'oc_platform',                      # Or path to database file if using sqlite3.
+        'USER': 'bobbyTM',                      # Not used with sqlite3.
+        'PASSWORD': 'LukeWildwood5054',                  # Not used with sqlite3.
+        'HOST': 'oc1.cnskgopefuhu.us-east-1.rds.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ocrootu',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'lifeme',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-"""
-import sys
-if 'test' in sys.argv:
-	DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ocrootutesting',
-        'USER': 'root',
-        'PASSWORD': 'lifeme',
-        'HOST': 'localhost',    
-        'PORT': '',    
-	}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -51,6 +27,10 @@ TIME_ZONE = 'America/New_York'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
+
+HAYSTACK_SITECONF = 'search_sites'
+HAYSTACK_SEARCH_ENGINE = 'solr'
+HAYSTACK_SOLR_URL = 'http://54.235.197.234:8983/solr'
 
 SITE_ID = 1
 
@@ -188,39 +168,10 @@ LOGGING = {
     }
 }
 
-HAYSTACK_SITECONF = 'search_sites'
-HAYSTACK_SEARCH_ENGINE = 'solr'
-HAYSTACK_SOLR_URL = 'http://54.235.197.234:8983/solr'
 
-from memcacheify import memcacheify
 
-CACHES = memcacheify()
 
-"""
-os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS', 'dev1.ec2.memcachier.com:11211').replace(',', ';')
-os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME', '12d7b2')
-os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD', '78d0da6ae9bb1409dcde')
-
-CACHES = {
-  'default': {
-    'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-    'LOCATION': os.environ.get('MEMCACHIER_SERVERS', 'dev1.ec2.memcachier.com:11211').replace(',', ';'),
-    'TIMEOUT': 500,
-    'BINARY': True,
-  }
-}
-"""
-
-INTERNAL_IPS = ('127.0.0.1',)
-
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
+try:
+	from settings_dev import *
+except ImportError:
+	pass
