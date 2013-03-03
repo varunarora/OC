68c2a296250f28b6cbe26d663e5d45b2ec378a5e
Varun Arora <varunarora.mct@gmail.com>
2013-03-02 19:50:24 -0500

    STATIC_ROOT path correction

--- oc_platform/settings.py
+++ oc_platform/settings.py
@@ -68,7 +68,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/django/static/'
+STATIC_ROOT = '/home/django/OC/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
