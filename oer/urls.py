59cfabfdf4ba5e9d89a43cf50adccf28103bfb7b
Varun Arora <varunarora.mct@gmail.com>
2013-04-28 13:38:41 -0400

    URL pattern for resource pages changed to only support ints, fixes Google bot /undefined/ error

--- oer/urls.py
+++ oer/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, url
 
 from oer import views
 
-urlpatterns = patterns('',
-	url(r'^download/(?P<resource_id>.+)/$', views.download, name='download'),
-	url(r'^(?P<resource_id>.+)/$', views.view_resource, name='read'),
+urlpatterns = patterns(
+    '',
+    url(r'^download/(?P<resource_id>.+)/$', views.download, name='download'),
+    url(r'^(?P<resource_id>\d+)/$', views.view_resource, name='read'),
 
-	url(r'^$', views.resource_center, name='resource_center'),
+    url(r'^$', views.resource_center, name='resource_center'),
 )
