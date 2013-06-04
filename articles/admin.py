1d6676ae5555b9b9845453a12c5f873b26c8c23d
Varun Arora <varunarora.mct@gmail.com>
2013-06-04 12:12:59 -0400

    Added multiple columns (for sortability) in admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from articles.models import Article, ArticleRevision
 
-admin.site.register(Article)
-admin.site.register(ArticleRevision)
+
+class ArticleRevisionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'article', 'flag', 'user', )
+    list_select_related = True
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published', 'category', 'revision')
+    list_select_related = True
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleRevision, ArticleRevisionAdmin)
