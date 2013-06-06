be6ffe527a26657967045660679d8cd0e56668f2
Varun Arora <varunarora.mct@gmail.com>
2013-06-05 21:08:09 -0400

    Pushing Interactions models for the first time

---
+++ interactions/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from articles.MarkdownTextField import MarkdownTextField
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+
+class Comment(models.Model):
+    body_markdown = MarkdownTextField()
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    user = models.ForeignKey(User)
+    parent_type = models.ForeignKey(ContentType)
+    parent_id = models.PositiveIntegerField()
+    parent = generic.GenericForeignKey('parent_type', 'parent_id')
+
+    def __unicode__(self):
+        return str(self.parent_type) + ": " + str(self.parent_id)
+
+"""
+class Favorite(models.Model):
+
+
+class Rating(models.Model):
+
+
+"""
