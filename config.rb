1480be48c99760d0a5f0a8bfffe0f8bbdfc8bacf
Varun Arora <varunarora.mct@gmail.com>
2013-06-06 18:24:45 -0400

    Initialize Compass settings

---
+++ config.rb
@@ -0,0 +1,21 @@
+# Require any additional compass plugins here.
+Compass::BrowserSupport.add_support("function-name", "webkit", "moz")
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "oc_platform/assets/css/"
+sass_dir = "oc_platform/assets/sass/"
+images_dir = "oc_platform/assets/images/"
+javascripts_dir = "oc_platform/assets/js/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :sass
