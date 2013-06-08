bc9ca2f7b33ad76906829223cf1450ea7643e39b
Varun Arora <varunarora.mct@gmail.com>
2013-06-07 22:23:30 -0400

    Function comments added and strings moved to settings for i18n

--- oc_platform/views.py
+++ oc_platform/views.py
@@ -1,58 +1,71 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.utils.translation import ugettext as _
+from django.conf import settings
 from articles.models import Article
 import json
 
 
 def home(request):
+    """Fetches the top articles, a count and the sign-in form"""
+    # Get the top 10 articles ordered in descending order of views.
     top_articles = Article.objects.order_by('title').order_by('-views')[:10]
+
+    # Get the count of the total number of articles in the database.
     article_count = Article.objects.all().count()
 
+    # Get the sign-in form.
     import SignupForm
     form = SignupForm.SignupForm()
 
+    # Fetch context variables by importing and invoking Google Plus function
+    #     in AuthHelper library.
     from user_account.AuthHelper import AuthHelper
-    context = dict(AuthHelper.generateGPlusContext(request).items() + {
-        'top_articles': top_articles, 'title': 'OpenCurriculum: A K-12 Learning Content Hub',
-        'count': article_count, 'form': form}.items())
+
+    context = dict(
+        AuthHelper.generateGPlusContext(request).items() + {
+            'top_articles': top_articles,
+            'title': _(settings.STRINGS['global']['TITLE']),
+            'count': article_count,
+            'form': form
+        }.items()
+    )
     return render(request, 'index.html', context)
 
 
 def t404(request):
-    top_articles = Article.objects.order_by('title')[:10]
-    context = {'top_articles': top_articles, 'title': 'OpenCurriculum: A K-12 Learning Content Hub'}
+    """Fetches and returns the 404 template for testing purposes"""
+    context = {'title': _(settings.STRINGS['global']['TITLE'])}
     return render(request, '404.html', context)
 
 
-def t505(request):
-    top_articles = Article.objects.order_by('title')[:10]
-    context = {'top_articles': top_articles, 'title': 'OpenCurriculum: A K-12 Learning Content Hub'}
+def t500(request):
+    """Fetches and returns the 500 template for testing purposes"""
+    context = {'title': _(settings.STRINGS['global']['TITLE'])}
     return render(request, '500.html', context)
 
 
 def contact(request):
-    top_articles = Article.objects.order_by('title')[:10]
-    context = {'top_articles': top_articles, 'title': 'OpenCurriculum: A K-12 Learning Content Hub'}
+    context = {'title': _(settings.STRINGS['global']['TITLE'])}
     return render(request, 'contact.html', context)
 
 
 def developers(request):
-    top_articles = Article.objects.order_by('title')[:10]
-    context = {'top_articles': top_articles, 'title': 'OpenCurriculum: A K-12 Textbook Hub'}
+    context = {'title': _(settings.STRINGS['global']['TITLE'])}
     return render(request, 'developers.html', context)
 
 
 def about(request):
-    context = {'title': 'About OpenCurriculum'}
+    context = {'title': _(settings.STRINGS['about']['TITLE'])}
     return render(request, 'about.html', context)
 
 
 def team(request):
-    context = {'title': 'OpenCurriculum\'s Team'}
+    context = {'title': _(settings.STRINGS['about']['team']['TITLE'])}
     return render(request, 'team.html', context)
 
 
 def press(request):
-    context = {'title': 'Press &lsaquo; OpenCurriculum'}
+    context = {'title': _(settings.STRINGS['about']['press']['TITLE'])}
     return render(request, 'press.html', context)
 
 
@@ -61,43 +74,65 @@ def help(request):
 
 
 def jobs(request):
-    context = {'title': 'Jobs @ OpenCurriculum'}
+    context = {'title': _(settings.STRINGS['jobs']['TITLE'])}
     return render(request, 'jobs.html', context)
 
 
 def terms(request):
-    context = {'title': 'Terms of Use &lsaquo; OpenCurriculum'}
+    context = {'title': _(settings.STRINGS['terms']['TITLE'])}
     return render(request, 'terms.html', context)
 
 
 def privacy(request):
-    context = {'title': 'Privacy Policy &lsaquo; OpenCurriculum'}
+    context = {'title': _(settings.STRINGS['privacy']['TITLE'])}
     return render(request, 'privacy.html', context)
 
 
 def license(request):
-    context = {'title': 'License &lsaquo; OpenCurriculum'}
+    context = {'title': _(settings.STRINGS['about']['license']['TITLE'])}
     return render(request, 'license.html', context)
 
 
-def signupinvite(request):
+def signup_invite(request):
+    """Sends sign-up invite request email to administrator(s)
+
+    Processes form input from POST dictionary objects, and if the data
+    validates, fetches the cleaned data from each of the fields. It then
+    contructs and dispatches the emails, and returns the success state of the
+    entire process
+
+    Returns:
+        JSON object response with 'status' and 'message' variables, with
+        'status' being a boolean and 'message' providing further information.
+        If validation failure, form error messages object based on the field
+        will be passed as 'message'
+    """
+    # Set up an empty dictionary response object.
     response = {}
+
     import SignupForm
+
     if request.method == "POST":
-        # TODO: Put a try here
+        # TODO(Varun): Put a try here.
         form = SignupForm.SignupForm(request.POST)
+
+        # If form validates, proceed with performing tasks.
         if form.is_valid():
+            # Fetch cleaned fields
             name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            purpose = form.cleaned_data['purpose']
+
+            # If the form requested an organization, assign, else set default.
             try:
                 organization = form.cleaned_data['organization']
             except:
                 organization = "NA"
-            email = form.cleaned_data['email']
-            purpose = form.cleaned_data['purpose']
 
-            from django.conf import settings
+            # Fetch the sign-up administrator emails from settings.
             recipients = settings.SIGNUPS_ADMINS
 
+            # Set simple subject line and message capturing cleaned fields.
             subject = "OC-Invite: " + purpose
             message = name + ", " + organization + ", " + email
 
@@ -106,61 +141,91 @@ def signupinvite(request):
                 send_mail(subject, message, email, recipients)
 
                 response['status'] = True
-                response['message'] = ('Congratulations! We have successfully received your '
-                                       'request submission.')
+                response['message'] = _(
+                    settings.STRINGS['global']['invite']['SUCCESS']
+                )
 
             except:
                 response['status'] = False
-                response['message'] = ('Unknown error occured. Try again or contact us at '
-                                       'hello@ for a resolution.')
+                response['message'] = _(
+                    settings.STRINGS['global']['invite']['FAILURE']
+                )
 
+        # If form did not validate, return errors.
         else:
             response['status'] = False
             response['message'] = form.errors
 
     return HttpResponse(json.dumps(response), content_type="application/json")
 
-# API Stuff below
 
+# API Stuff below.
 
 def get_breadcrumb(request):
+    """Builds and returns the breadcrumb of a category from its ID
+
+    Returns:
+        Serialized category breadcrumb as a JSON object. If category cannot be
+        located, a simple string message reporting the failure
+    """
     category_id = request.GET.get('category_id', '')
 
     try:
+        # Fetch the category from the ID provided in the GET request.
         from meta.models import Category
         category = Category.objects.get(pk=category_id)
 
+        # Build the breadcrumb by using the appropriate class.
         from articles.ArticleUtilities import ArticleUtilities
         breadcrumb = ArticleUtilities.buildBreadcrumb(category)
+        # Reverse it to begin with root element.
         breadcrumb.reverse()
 
+        # Manually serialize the breadcrumb by iterating through it.
         serializableBreadcrumb = []
-
         for bc in breadcrumb:
             serializableBreadcrumb.append(bc.title)
 
         return HttpResponse(
-            json.dumps(serializableBreadcrumb), 200, content_type="application/json")
+            json.dumps(serializableBreadcrumb), 200,
+            content_type="application/json"
+        )
 
     except:
         return HttpResponse(
-            json.dumps('Failed to locate category'), 401, content_type="application/json")
+            json.dumps(
+                _(settings.STRINGS['meta']['category']['API_LOCATION_FAILURE'])
+            ), 401,
+            content_type="application/json"
+        )
 
 
 def email_share(request):
+    """Dispatched a 'share' email to an email address
+
+    Returns:
+        JSON object with a key of 'status' with JavaScript boolean
+        representation of success of operation
+    """
+    # Fetch POST fields.
     email_addresses = request.POST.get('email')
     message = request.POST.get('message')
     from_name = request.POST.get('from_name')
-    # TODO: Do something with the user provided email address
+
+    # TODO(Varun): Do something with the user provided email address.
     # from_address = request.POST.get('from_address')
 
     try:
+        # Build a list of email addresses, if multiple comma-separated values
+        #     provided.
         email_addresses = email_addresses.split(',')
 
         from django.core.mail import send_mail
         send_mail(
-            from_name + ' shared an article with you', message,
-            from_name + ' <info@theopencurriculum.org>', email_addresses, fail_silently=False)
+            from_name + ' ' + _(settings.STRINGS['share']['SUBJECT_APPEND']),
+            message, from_name + ' <%s>' % settings.SERVER_EMAIL,
+            email_addresses, fail_silently=False
+        )
 
         status = {'status': 'true'}
 
