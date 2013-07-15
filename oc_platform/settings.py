85849a433f50ae35275888d1059deb03f9ef4afe
Varun Arora <varunarora.mct@gmail.com>
2013-07-15 18:01:32 -0400

    Changes to static and media URLs to match server dir structure

--- oc_platform/settings.py
+++ oc_platform/settings.py
@@ -70,7 +70,7 @@ ABSOLUTE_PATH = lambda x: os.path.join(os.path.abspath(TEMPLATE_DIR), x)
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -90,7 +90,7 @@ TEMP_IMAGE_DIR = STATIC_ROOT + 'images/tmp/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static/assets/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -250,7 +250,8 @@ STRINGS = {
         'HISTORY_TITLE': 'Edit history for %s &lsaquo; OpenCurriculum',
         'messages': {
             'SUBMIT_REVIEW': 'Your edits have been successfully submitted for review',
-            'EDITS_SAVE': 'Successfully saved edits at %s'
+            'EDITS_SAVE': 'Successfully saved edits at %s',
+            'ANONYMOUS_EDITING': 'Editing as anonymous.'
         }
     },
     'user': {
