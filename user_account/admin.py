532043b416746abb7e47c9b0c014e8f35671d4f8
Varun Arora <varunarora.mct@gmail.com>
2013-07-18 10:50:50 -0400

    Add cohort and notifications models to the admin

--- user_account/admin.py
+++ user_account/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from user_account.models import UserProfile
+from user_account.models import UserProfile, Cohort, Notification
 
 admin.site.register(UserProfile)
+admin.site.register(Cohort)
+admin.site.register(Notification)
