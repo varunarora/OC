ec6137e414203c0a207f96708ac08a762e5121b1
Srinivasan Vijayaraghavan <srinivav@andrew.cmu.edu>
2013-07-24 20:45:21 -0400

    Fix jQuery click handler.

--- oc_platform/assets/js/profile.js
+++ oc_platform/assets/js/profile.js
@@ -6,14 +6,15 @@
  /* global $, OC */
 
  /* When the subscribe button is clicked, send async POST to the server */
- $('.subscribe').one('click', function() {
-    alert("HI!");
-    var data = {
-        'id': OC.profile.id,
-    };
+$(document).ready(function() {
+    $('.subscribe').on('click', function() {
+        var data = {
+            'id': OC.profile.id,
+        };
 
-    /* When the server responds, change the text */
-    $.post('/api/subscribe', data, function() {
-        alert('success!');
+        /* When the server responds, change the text */
+        $.post('/api/subscribe/', data, function() {
+            alert('success!');
+        });
     });
- });
+});
