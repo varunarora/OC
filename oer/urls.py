3ce70ee9a16e6dc9096d32517963b6ceb9da0595
Varun Arora <varunarora.mct@gmail.com>
2013-08-06 12:03:47 -0400

    Create OER urls

--- oer/urls.py
+++ oer/urls.py
@@ -5,6 +5,20 @@ from oer import views
 urlpatterns = patterns(
     '',
     url(r'^download/(?P<resource_id>.+)/$', views.download, name='download'),
+    url(r'^add-video/$', views.add_video, name='add_video'),
+    url(r'^add-link/$', views.add_url, name='add_url'),
+    url(r'^new-document/$', views.new_document, name='new_document'),
+
+    url(r'^create-video/$', views.create_video, name='create_video'),
+    url(r'^create-link/$', views.create_url, name='create_url'),
+
+    # Collections
+    url(r'^new-collection/project/(?P<project_slug>[\w\-]+)/', views.new_project_collection, name='new_project_collection'),
+    url(r'^new-collection/user/(?P<username>\w+)/', views.new_user_collection, name='new_user_collection'),
+
+    # Resources API
+    url(r'^delete-resource/(?P<resource_id>.+)/', views.delete_resource, name='delete_resource'),
+
     url(r'^(?P<resource_id>\d+)/$', views.view_resource, name='read'),
 
     url(r'^$', views.resource_center, name='resource_center'),
