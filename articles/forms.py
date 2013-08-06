08d7063bb675882c2b3f9288b4bc67f57557f30b
Varun Arora <varunarora.mct@gmail.com>
2013-08-06 12:12:44 -0400

    Shift from tag creation internally to delegated to CollectionUtilities

--- articles/forms.py
+++ articles/forms.py
@@ -1,11 +1,9 @@
 from django.forms import ModelForm
 from articles.models import ArticleRevision
+from oc_platform import FormUtilities
 
 
 class EditArticleForm(ModelForm):
-    from django import forms
-    #tags = forms.CharField(required=False)
-
     def __init__(self, request, user, article, flag):
         newRequest = request.copy()
 
@@ -20,31 +18,12 @@ class EditArticleForm(ModelForm):
         newRequest.__setitem__('article', article.id)
         newRequest.__setitem__('flag', flag)
 
-        tags = self.convert_to_tag_array(newRequest.getlist('tags'))
-        if len(tags) == 0:
-            newRequest.setlist('tags', [])
-        else:
-            newRequest.setlist('tags', tags)
+        from meta.models import TagCategory
+        tag_category = TagCategory.objects.get(title='Article')
+        newRequest.setlist('tags', FormUtilities.get_taglist(
+            newRequest.getlist('tags'), tag_category))
 
         super(EditArticleForm, self).__init__(newRequest)
-
-    @staticmethod
-    def convert_to_tag_array(tag_array):
-        from meta.models import Tag
-
-        tags = []
-
-        for tag in tag_array:
-            try:
-                present_tag = Tag.objects.get(title=tag)
-                tags.append(present_tag.id)
-            except:
-                # Create new tag and return it
-                new_tag = Tag(title=tag, description='')
-                new_tag.save()
-                tags.append(new_tag.id)
-
-        return tags
 
     class Meta:
         model = ArticleRevision
