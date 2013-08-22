a31a862ca4c7edd1fcce9f49522d56449b7cfb39
Varun Arora <varunarora.mct@gmail.com>
2013-08-22 16:12:06 -0400

    Support for editing resources. Fixes #7328527412793

--- oer/forms.py
+++ oer/forms.py
@@ -48,6 +48,88 @@ class NewURLForm(forms.ModelForm):
         model = Resource
 
 
+class URLEditForm(forms.ModelForm):
+    def __init__(self, request, instance):
+        newRequest = request.copy()
+
+        # For now, default to the previous values of user, type & cost
+        newRequest.setdefault('user', instance.user.id)
+        newRequest.setdefault('type', instance.type)
+        newRequest.setdefault('cost', instance.cost)        
+
+        from meta.models import TagCategory
+        tag_category = TagCategory.objects.get(title='URL')
+        newRequest.setlist('tags', FormUtilities.get_taglist(
+            newRequest.getlist('tags'), tag_category))
+
+        super(URLEditForm, self).__init__(newRequest, instance=instance)
+
+    class Meta:
+        model = Resource
+
+
+class VideoEditForm(forms.ModelForm):
+    def __init__(self, request, instance):
+        newRequest = request.copy()
+
+        # For now, default to the previous values of user, type & cost
+        newRequest.setdefault('user', instance.user.id)
+        newRequest.setdefault('type', instance.type)
+        newRequest.setdefault('cost', instance.cost)        
+
+        from meta.models import TagCategory
+        tag_category = TagCategory.objects.get(title='Resources')
+        newRequest.setlist('tags', FormUtilities.get_taglist(
+            newRequest.getlist('tags'), tag_category))
+
+        super(VideoEditForm, self).__init__(newRequest, instance=instance)
+
+    class Meta:
+        model = Resource
+
+
+class DocumentEditForm(forms.ModelForm):
+    def __init__(self, request, instance):
+        newRequest = request.copy()
+
+        # For now, default to the previous values of user, type & cost
+        newRequest.setdefault('user', instance.user.id)
+        newRequest.setdefault('type', instance.type)
+        newRequest.setdefault('cost', instance.cost)        
+
+        from meta.models import TagCategory
+        tag_category = TagCategory.objects.get(title='Document')
+        newRequest.setlist('tags', FormUtilities.get_taglist(
+            newRequest.getlist('tags'), tag_category))
+
+        super(DocumentEditForm, self).__init__(newRequest, instance=instance)
+
+    class Meta:
+        model = Resource
+
+
+class AttachmentEditForm(forms.ModelForm):
+    def __init__(self, request, instance):
+        newRequest = request.copy()
+
+        # For now, default to the previous values of user, type & cost
+        newRequest.setdefault('user', instance.user.id)
+        newRequest.setdefault('type', instance.type)
+        newRequest.setdefault('cost', instance.cost)
+        newRequest.setdefault('file', instance.file)
+        newRequest.setdefault('url', instance.url)
+
+        from meta.models import TagCategory
+        tag_category = TagCategory.objects.get(title='Resources')
+        newRequest.setlist('tags', FormUtilities.get_taglist(
+            newRequest.getlist('tags'), tag_category))
+
+        super(AttachmentEditForm, self).__init__(newRequest, instance=instance)
+
+    class Meta:
+        model = Resource
+
+
 class NewDocumentForm(forms.ModelForm):
     def __init__(self, request, user):
         newRequest = request.copy()
