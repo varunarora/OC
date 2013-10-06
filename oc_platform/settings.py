736fc9d53cf67e79cacaca83348cad3cef3aaff3
Varun Arora <varunarora.mct@gmail.com>
2013-10-06 15:22:19 -0400

    Temporary resetting procurement of media files to s3fs

--- oc_platform/settings.py
+++ oc_platform/settings.py
@@ -72,7 +72,7 @@ PROJECT_PATH = '/home/django/OC/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://ocmedia.s3.amazonaws.com/' #'/static/media/'
+MEDIA_URL =  '/static/media/' #'http://ocmedia.s3.amazonaws.com/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
