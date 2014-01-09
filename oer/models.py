d37a7554aeb8c6e69d49b05480e57dfbe82580fa
Varun Arora <varunarora.mct@gmail.com>
2014-01-09 00:25:31 -0800

    Introduction of new models in oer in non-public facing way

--- oer/models.py
+++ oer/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from license.models import License
+from meta.models import Language
+from articles.jsonfield.fields import JSONField
 from articles.MarkdownTextField import MarkdownTextField
 from ResourceThumbnail import ResourceThumbnail
 from django.contrib.contenttypes.models import ContentType
@@ -10,6 +12,10 @@ from django.dispatch import Signal
 
 def get_default_license():
     return License.objects.get(id=1)
+
+
+def get_default_language():
+    return Language.objects.get(pk=1)
 
 
 class Resource(models.Model):
@@ -28,7 +34,30 @@ class Resource(models.Model):
     image = models.ImageField(upload_to='resource_thumbnail', blank=True)
     source = models.CharField(max_length=256, null=True, blank=True)
     collaborators = models.ManyToManyField(User, blank=True, null=True,
+        related_name='collabsy')
+
+
+class ResourceNew(models.Model):
+    title = models.CharField(max_length=256)
+    license = models.ForeignKey('license.License', default=get_default_license)
+    revision = models.ForeignKey('ResourceRevision', related_name="current_revision")
+    user = models.ForeignKey(User)
+    tags = models.ManyToManyField('meta.Tag', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    visibility = models.CharField(max_length=256)
+    cost = models.FloatField()
+    views = models.IntegerField(editable=False, default=0)
+    image = models.ImageField(upload_to='resource_thumbnail', null=True, blank=True)
+    source = models.CharField(max_length=256, null=True, blank=True)
+    collaborators = models.ManyToManyField(User, blank=True, null=True,
         related_name='collabs')
+    viewers = models.ManyToManyField(User, blank=True, null=True, related_name='viewers')
+    category = models.ForeignKey('meta.Category', null=True, blank=True)
+    language = models.ForeignKey('meta.Language', default=get_default_language)
+    slug = models.SlugField(max_length=256)
+    stage = models.CharField(max_length=64, null=True, blank=True)
+
 
     def __unicode__(self):
         return self.title
@@ -68,3 +97,43 @@ class Collection(models.Model):
         return self.title
     
     collaborator_added = Signal(providing_args=["collection", "user"])
+
+
+class Forks(models.Model):
+    resource = models.ForeignKey('Resource', related_name='resource')
+    fork_of = models.ForeignKey('Resource', related_name='fork_of')
+    flag = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+class ResourceRevision(models.Model):
+    resource = models.ForeignKey('Resource', null=True, blank=True)    
+    content_type = models.ForeignKey(ContentType)
+    content_id = models.PositiveIntegerField()
+    content = generic.GenericForeignKey('content_type', 'content_id')
+    log = models.CharField(max_length=256, null=True, blank=True)
+    user = models.ForeignKey(User, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+class Element(models.Model):
+    body = JSONField()
+
+
+class Document(models.Model):
+    elements = models.ManyToManyField(
+        'oer.Element', null=True, blank=True, through='DocumentElement')
+
+
+class DocumentElement(models.Model):
+    document = models.ForeignKey(Document)
+    element = models.ForeignKey(Element)
+    position = models.PositiveIntegerField()
+
+
+class Link(models.Model):
+    url = models.URLField(null=True, blank=True)
+
+
+class Attachment(models.Model):
+    file = models.FileField(upload_to='resources', null=True, blank=True)
