9deede4ba4d7e3898af13a1210b18fe5d4065d2b
Varun Arora <varunarora.mct@gmail.com>
2014-04-11 02:21:24 -0700

    Follow on support for new lesson types

--- oc_platform/assets/js/ckeditor/plugins/lesson/plugin.js
+++ oc_platform/assets/js/ckeditor/plugins/lesson/plugin.js
@@ -89,7 +89,7 @@ CKEDITOR.plugins.add('lesson', {
 
         editor.widgets.add('threeActLessonPlan', {
             allowedContent: 'div(!lesson-content); ' +
-                'div(!lesson-act-one-resource-suggestion); div(!alesson-ct-one-body); ' +
+                'div(!lesson-act-one-resource-suggestion); div(!lesson-act-one-body); ' +
                 'div(!lesson-act-two-resource-suggestion); div(!lesson-act-two-body); ' +
                 'div(!lesson-act-three-resource-suggestion); div(!lesson-act-three-body); ' +
                 'div(!lesson-sequel-resource-suggestion); div(!lesson-sequel-body); ',
@@ -121,5 +121,141 @@ CKEDITOR.plugins.add('lesson', {
             }
         });
 
+
+        editor.widgets.add('ubdLessonPlan', {
+            allowedContent: 'div(!lesson-content); ' +
+                'div(!lesson-established-goals); div(!lesson-established-goals-body); ' +
+                'div(!lesson-understandings-questions-wrapper);' +
+                'div(!lesson-understandings); div(!lesson-understandings-body); ' +
+                'div(!lesson-questions); div(!lesson-questions-body); ' +
+                'div(!lesson-knowledge-ability-wrapper);' +
+                'div(!lesson-knowledge); div(!lesson-knowledge-body); ' +
+                'div(!lesson-ability); div(!lesson-ability-body); ' +
+                'div(!lesson-performance-evidence-wrapper);' +
+                'div(!lesson-performance); div(!lesson-performance-body); ' +
+                'div(!lesson-evidence); div(!lesson-evidence-body); ' +
+                'div(!lesson-activities); div(!lesson-activities-body); ',
+            upcast: function(element) {
+                return element.name == 'div' && (
+                    element.hasClass('lesson-established-goals') ||
+                    element.hasClass('lesson-understandings') ||
+                    element.hasClass('lesson-questions') ||
+                    element.hasClass('lesson-knowledge') ||
+                    element.hasClass('lesson-ability') ||
+                    element.hasClass('lesson-performance') ||
+                    element.hasClass('lesson-evidence') ||
+                    element.hasClass('lesson-activities')
+                );
+            },
+            editables: {
+                goals: {
+                    selector: '.lesson-established-goals-body'
+                },
+                understandings: {
+                    selector: '.lesson-understandings-body'
+                },
+                essentialQuestions: {
+                    selector: '.lesson-questions-body'
+                },
+                knowledge: {
+                    selector: '.lesson-knowledge-body'
+                },
+                ability: {
+                    selector: '.lesson-ability-body'
+                },
+                performance: {
+                    selector: '.lesson-performance-body'
+                },
+                evidence: {
+                    selector: '.lesson-evidence-body'
+                },
+                activities: {
+                    selector: '.lesson-activities-body'
+                },
+            }
+        });
+
+
+        editor.widgets.add('simpleLessonPlan', {
+            allowedContent: 'div(!lesson-content); ' +
+                'div(!lesson-question-body); div(!lesson-objectives-body); ' +
+                'div(!lesson-activation-body); div(!lesson-activity-body); ' +
+                'div(!lesson-guided-practice-body); div(!lesson-independant-practice-body); ' +
+                'div(!lesson-assessment-body); div(!lesson-reflection-body); ',
+            upcast: function(element) {
+                return element.name == 'div' && (
+                    element.hasClass('lesson-question-body') ||
+                    element.hasClass('lesson-objectives-body') ||
+                    element.hasClass('lesson-activation-body') ||
+                    element.hasClass('lesson-activity-body') ||
+                    element.hasClass('lesson-guided-practice-body') ||
+                    element.hasClass('lesson-independant-practice-body') ||
+                    element.hasClass('lesson-assessment-body') ||
+                    element.hasClass('lesson-reflection-body')
+                );
+            },
+            editables: {
+                question: {
+                    selector: '.lesson-question-body'
+                },
+                objectives: {
+                    selector: '.lesson-objectives-body'
+                },
+                activation: {
+                    selector: '.lesson-activation-body'
+                },
+                activity: {
+                    selector: '.lesson-activity-body'
+                },
+                guidedPractice: {
+                    selector: '.lesson-guided-practice-body'
+                },
+                independantPractice: {
+                    selector: '.lesson-independant-practice-body'
+                },
+                assessment: {
+                    selector: '.lesson-assessment-body'
+                },
+                reflection: {
+                    selector: '.lesson-reflection-body'
+                },
+            }
+        });
+
+        /*
+        editor.widgets.add('weeklyLessonPlan', {
+            allowedContent: 'div(!lesson-content); table(!lesson-weekly-table); tr; th;' +
+                'td(!lesson-objectives-body); td(!lesson-do-now-body); ' +
+                'td(!lesson-introduction-materials-body); td(!lesson-guided-practice-body); ' +
+                'td(!lesson-independant-practice-body); td(!lesson-exit-body); ' +
+                'td(!lesson-modifications-body);',
+            upcast: function(element) {
+                return element.name == 'td';
+            },
+            editables: {
+                objectives: {
+                    selector: '.lesson-objectives-body'
+                },
+                doNow: {
+                    selector: '.lesson-do-now-body'
+                },
+                introductionMaterials: {
+                    selector: '.lesson-introduction-materials-body'
+                },
+                guidedPractice: {
+                    selector: '.lesson-guided-practice-body'
+                },
+                independantPractice: {
+                    selector: '.lesson-independant-practice-body'
+                },
+                exitTicket: {
+                    selector: '.lesson-exit-body'
+                },
+                modifications: {
+                    selector: '.lesson-modifications-body'
+                }
+            }
+        });
+        */
     }
 });
