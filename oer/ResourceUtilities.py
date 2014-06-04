6de5ea3b3a3deed505bb012f5df7f9d5f6cb87e8
Varun Arora <varunarora.mct@gmail.com>
2014-06-04 03:01:01 -0700

    Wrap attempt to find resource type on link in try

--- oer/ResourceUtilities.py
+++ oer/ResourceUtilities.py
@@ -17,33 +17,36 @@ def get_resource_root(resource):
 
 def get_resource_type_from_url(resource):
     import urlparse
-    (hostname, url_data) = get_url_hostname(resource.revision.content.url)
+    try:
+        (hostname, url_data) = get_url_hostname(resource.revision.content.url)
 
-    # If the resource is a video, determine whether or not it is a YouTube
-    #     Vimeo video, and obtain the video ID (as determined by the
-    #     service provider), so that it can be plugged into its player.
+        # If the resource is a video, determine whether or not it is a YouTube
+        #     Vimeo video, and obtain the video ID (as determined by the
+        #     service provider), so that it can be plugged into its player.
 
-    # In either case, use an appropriate pattern matching to obtain the
-    #     video #.
-    if "youtube" in hostname:
-        query = urlparse.parse_qs(url_data.query)
-        video = query["v"][0]
-        resource.video_tag = video
-        resource.provider = "youtube"
-        resource_type = "video"
+        # In either case, use an appropriate pattern matching to obtain the
+        #     video #.
+        if "youtube" in hostname:
+            query = urlparse.parse_qs(url_data.query)
+            video = query["v"][0]
+            resource.video_tag = video
+            resource.provider = "youtube"
+            resource_type = "video"
 
-    elif "vimeo" in hostname:
-        resource.video_tag = url_data.path.split('/')[1]
-        resource.provider = "vimeo"
-        resource_type = "video"
+        elif "vimeo" in hostname:
+            resource.video_tag = url_data.path.split('/')[1]
+            resource.provider = "vimeo"
+            resource_type = "video"
 
-    elif ('docs' in hostname and 'google' in hostname) and (
-        'presentation' in url_data.path):
-        resource.revision.content.url = resource.revision.content.url.replace(
-            'pub', 'embed')
-        resource_type = 'gpres'
+        elif ('docs' in hostname and 'google' in hostname) and (
+            'presentation' in url_data.path):
+            resource.revision.content.url = resource.revision.content.url.replace(
+                'pub', 'embed')
+            resource_type = 'gpres'
 
-    else:
+        else:
+            resource_type = "url"
+    except:
         resource_type = "url"
 
     return resource_type
