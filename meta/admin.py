0ac8bbd95345e688eb623a271012e47c15e3929f
Varun Arora <varunarora.mct@gmail.com>
2014-06-10 20:58:01 -0700

    Show tagmapping in admin

--- meta/admin.py
+++ meta/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from meta.models import Language, Category, Tag, TagCategory, TagLabel
+from meta.models import Language, Category, Tag, TagMapping, TagCategory, TagLabel
 
 admin.site.register(Language)
 admin.site.register(Category)
 admin.site.register(Tag)
+admin.site.register(TagMapping)
 admin.site.register(TagCategory)
 admin.site.register(TagLabel)
