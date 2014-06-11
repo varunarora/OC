bf976d9d9dadd26e9e92c1619f5403a2206da227
Varun Arora <varunarora.mct@gmail.com>
2014-06-10 20:57:23 -0700

    Attempt fb login and gracefully degrade if non-footer page

--- oc_platform/assets/js/core.js
+++ oc_platform/assets/js/core.js
@@ -4319,53 +4319,57 @@ jQuery(document).ready(function ($) {
         allowSpaces: true
     });
 
-    // Function to initialize the Facebook button.
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : fbAppID,
-            status     : true, // check login status
-            cookie     : true, // enable cookies to allow the server to access the session
-            xfbml      : true  // parse XFBML
-        });
+    try {
+        // Function to initialize the Facebook button.
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : fbAppID,
+                status     : true, // check login status
+                cookie     : true, // enable cookies to allow the server to access the session
+                xfbml      : true  // parse XFBML
+            });
 
-        FB.Event.subscribe('auth.authResponseChange', function(response) {
-            var registrationCover = $('.registration-cover');
-            if (registrationCover.length > 0 && registrationCover.attr('title') === 'facebook'){
-                OC.facebookRegistrationCallback({'status': 'false'});
-            }
-        });
-        /*
-        // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
-        // for any authentication related change, such as login, logout or session refresh. This means that
-        // whenever someone who was previously logged out tries to log in again, the correct case below 
-        // will be handled. 
-        FB.Event.subscribe('auth.authResponseChange', function(response) {
-            // Here we specify what we do with the response anytime this event occurs. 
-            if (response.status === 'connected') {
-              // The response object is returned with a status field that lets the app know the current
-              // login status of the person. In this case, we're handling the situation where they 
-              // have logged in to the app.
-              //OC.facebookRegistrationCallback();
-            } else if (response.status === 'not_authorized') {
-              // In this case, the person is logged into Facebook, but not into the app, so we call
-              // FB.login() to prompt them to do so. 
-              // In real-life usage, you wouldn't want to immediately prompt someone to login 
-              // like this, for two reasons:
-              // (1) JavaScript created popup windows are blocked by most browsers unless they 
-              // result from direct interaction from people using the app (such as a mouse click)
-              // (2) it is a bad experience to be continually prompted to login upon page load.
-              FB.login();
-            } else {
-              // In this case, the person is not logged into Facebook, so we call the login() 
-              // function to prompt them to do so. Note that at this stage there is no indication
-              // of whether they are logged into the app. If they aren't then they'll see the Login
-              // dialog right after they log in to Facebook. 
-              // The same caveats as above apply to the FB.login() call here.
-              FB.login();
-            }
-        });
-        */
-    };
+            FB.Event.subscribe('auth.authResponseChange', function(response) {
+                var registrationCover = $('.registration-cover');
+                if (registrationCover.length > 0 && registrationCover.attr('title') === 'facebook'){
+                    OC.facebookRegistrationCallback({'status': 'false'});
+                }
+            });
+            /*
+            // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+            // for any authentication related change, such as login, logout or session refresh. This means that
+            // whenever someone who was previously logged out tries to log in again, the correct case below 
+            // will be handled. 
+            FB.Event.subscribe('auth.authResponseChange', function(response) {
+                // Here we specify what we do with the response anytime this event occurs. 
+                if (response.status === 'connected') {
+                  // The response object is returned with a status field that lets the app know the current
+                  // login status of the person. In this case, we're handling the situation where they 
+                  // have logged in to the app.
+                  //OC.facebookRegistrationCallback();
+                } else if (response.status === 'not_authorized') {
+                  // In this case, the person is logged into Facebook, but not into the app, so we call
+                  // FB.login() to prompt them to do so. 
+                  // In real-life usage, you wouldn't want to immediately prompt someone to login 
+                  // like this, for two reasons:
+                  // (1) JavaScript created popup windows are blocked by most browsers unless they 
+                  // result from direct interaction from people using the app (such as a mouse click)
+                  // (2) it is a bad experience to be continually prompted to login upon page load.
+                  FB.login();
+                } else {
+                  // In this case, the person is not logged into Facebook, so we call the login() 
+                  // function to prompt them to do so. Note that at this stage there is no indication
+                  // of whether they are logged into the app. If they aren't then they'll see the Login
+                  // dialog right after they log in to Facebook. 
+                  // The same caveats as above apply to the FB.login() call here.
+                  FB.login();
+                }
+            });
+            */
+        };
+    } catch (e) {
+
+    }
 
     // Load the SDK asynchronously
     (function(d){
@@ -4376,6 +4380,7 @@ jQuery(document).ready(function ($) {
        ref.parentNode.insertBefore(js, ref);
     }(document));
 });
+var fbAppID;
 
 $(document).ajaxSend(function (event, xhr, settings) {
     function getCookie(name) {
