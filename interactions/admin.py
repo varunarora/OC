99091545c29a67fb0fe16282fbe68259168e3e4c
Varun Arora <varunarora.mct@gmail.com>
2014-06-17 19:06:29 -0700

    Admin on review model

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from interactions.models import Comment, CommentReference, Vote, Favorite
+from interactions.models import Comment, CommentReference, Vote, Favorite, Review
 
 admin.site.register(Comment)
 admin.site.register(CommentReference)
 admin.site.register(Vote)
 admin.site.register(Favorite)
+admin.site.register(Review)
