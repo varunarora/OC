12c0be3c3f94dba590f526e7464a830fe09b24ac
Varun Arora <varunarora.mct@gmail.com>
2014-08-17 14:20:28 -0700

    Remove project browse urls

--- projects/urls.py
+++ projects/urls.py
@@ -21,9 +21,6 @@ urlpatterns = patterns(
     url(r'^(?P<project_slug>[\w\-]+)/discussions/$', views.discussions, name='project_discussions'),
     url(r'^(?P<project_slug>[\w\-]+)/discussions/post/$', views.post_discussion, name='post_discussion'),
 
-    url(r'^(?P<project_slug>[\w\-]+)/browse/(?P<collection_slug>.+)/$', views.list_collection, name='list_collection'),
-    url(r'^(?P<project_slug>[\w\-]+)/browse/', views.browse, name='project_browse'),
-
     url(r'^(?P<project_id>\d+)/add/(?P<user_id>\d+)/$', views.add_member, name='add_member'),
     url(r'^(?P<project_id>\d+)/remove/(?P<user_id>\d+)/$', views.remove_member, name='remove_member'),
     url(r'^(?P<project_id>\d+)/add-admin/(?P<user_id>\d+)/$', views.add_admin, name='add_admin'),
