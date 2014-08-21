443b0a769f2a828a96680e3e105477baa9e36bef
Varun Arora <varunarora.mct@gmail.com>
2014-08-21 02:51:55 -0700

    Enable debug toolbar on production

--- oc_platform/settings.py
+++ oc_platform/settings.py
@@ -367,9 +367,25 @@ STRINGS = {
 }
 
 STAR_USERS = [
-    'khanacademy', 'ck12', 'joshua', 'ddmeyer', 'illustrativemathematics',
+    'khanacademy', 'ck12', 'joshua', 'mars', 'ddmeyer', 'illustrativemathematics',
     'engageny', 'yummymath', 'fawnpnguyen', 'samjshah', 'ericpollock'
 ]
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False
+}
 
 try:
     from settings_dev import *
