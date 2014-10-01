c0bd4d0f039e0e40acd6edf592aecb9b94b2fda0
Varun Arora <varunarora.mct@gmail.com>
2014-09-30 18:35:08 -0700

    Init TFI math objective standards

---
+++ tfi-tags.py
@@ -0,0 +1,30 @@
+from oc_platform import settings
+from django.core.management import setup_environ
+setup_environ(settings)
+
+import csv
+
+from meta.models import Tag, TagCategory
+category = TagCategory.objects.get(title='Objectives')
+
+tags_raw = open('tfi-math-objectives.csv', 'r')
+tag_rows = csv.reader(tags_raw, delimiter=',')
+
+for row in tag_rows:
+    tag_raw = row
+    standard = '.'.join(tag_raw[:6]).upper()
+    description = tag_raw[6].strip()
+    
+    try:
+        tag = Tag.objects.get(title=standard)
+        tag.category = category
+        tag.save()
+
+    except:
+        print standard
+        tag = Tag(
+            title=standard,
+            description=description,
+            category=category
+        )
+        tag.save()
