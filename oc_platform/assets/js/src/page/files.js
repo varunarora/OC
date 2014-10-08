22b294ea05bf382bf9f8c2c37fb79cde1b77d3c5
Varun Arora <varunarora.mct@gmail.com>
2014-10-08 02:31:50 -0700

    Scroll bar on user collection navigation

--- oc_platform/assets/js/src/page/files.js
+++ oc_platform/assets/js/src/page/files.js
@@ -1,2 +1,20 @@
-define(['jquery', 'core', 'resourcesCollections'], function($, OC){
+define(['jquery', 'core', 'resourcesCollections', 'nanoscroller'], function($, OC){
+    $(document).ready(function($){
+        function resizeFolderNavigation(){
+            $('nav.collections-navigation').height(
+                $('.category-panel').height() - $('.user-profile-info').outerHeight(true) - 20
+            );
+        }
+
+        resizeFolderNavigation();
+        $(window).resize(resizeFolderNavigation);
+
+        $('nav.collections-navigation > ul').addClass('scroll-content');
+        $('nav.collections-navigation').nanoScroller({
+            paneClass: 'scroll-pane',
+            sliderClass: 'scroll-slider',
+            contentClass: 'scroll-content',
+            flash: true
+        });
+    });
 });
